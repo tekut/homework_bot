a89c75f60f17d417e029ba86c74383d2aaffbd61
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-07-11 16:02:54 +0100

    WiP

--- homework.py
+++ homework.py
@@ -1,12 +1,19 @@
-...
+import telegram
+import os
+from telegram import Bot
+from telegram.ext import Updater
+import requests
+import logging
+from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
 
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -20,36 +27,54 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверяет доступность переменных окружения,
+    которые необходимы для работы программы."""
+    var_names = ["PRACTICUM_TOKEN", "TELEGRAM_TOKEN", "TELEGRAM_CHAT_ID"]
+    var_values = [os.getenv(var_name) for var_name in var_names]
+    for var_name, var_value in zip(var_names, var_values):
+        print(f"Переменная окружения {var_name} недоступна.")
+    else:
+        print(f"Значение переменной окружения {var_name}: {var_value}")
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщение в Telegram чат"""
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.debug('Сообщение отправлено '
+                      f'{TELEGRAM_CHAT_ID}: {message}')
+        logger.debug('Сообщение отправлено ')
+    except Exception as error:
+        logging.error(f'Ошибка при отправке сообщения: {error}')
 
 
 def get_api_answer(timestamp):
-    ...
+    """Делает запрос к Практикуму"""
+    payload = {'from_date': timestamp}
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+    response = homework_statuses.json()
+    return response
 
-
-def check_response(response):
-    ...
+#def check_response(response):
+#    """Проверяет ответ API на соответствие документации"""
+#    ...
 
 
 def parse_status(homework):
-    ...
-
+    """Извлекает из информации о конкретной
+    домашней работе статус этой работы"""
+    homework_name = homework['homework_name']
+    status_homework = homework['status']
+    verdict = HOMEWORK_VERDICTS[status_homework]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
 
-    ...
+
 
     while True:
         try:
